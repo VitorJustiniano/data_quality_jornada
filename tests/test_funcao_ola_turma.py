5f112add698dc2a060cda2e01227fdb3e0c75a30
Vitor Justiniano <vitor.mjustiniano@hotmail.com>
2026-07-03 22:49:24 -0300

    update

--- tests/test_funcao_ola_turma.py
+++ tests/test_funcao_ola_turma.py
@@ -1,4 +1,6 @@
 from app.funcao import funcao_ola_turma
 
-def test_funcao_ola_turma():
-    funcao_ola_turma()
+def test_funcao_ola_turma_retorna_ola_jornada():
+    saida = funcao_ola_turma()
+    gabarito = "Olá, turma da jornada de qualidade de dados!"
+    assert saida == gabarito
